0f00523ca88c48cbef183bbd2d257f8b4f34da2d
sean <zerojetlag@gmail.com>
2015-05-02 21:40:16 +0800

    init

---
+++ BatteryMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatteryMonitor
+//
+//  Created by sean on 15-5-2.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BatteryMonitorTests/BatteryMonitorTests.m
@@ -0,0 +1,40 @@
+//
+//  BatteryMonitorTests.m
+//  BatteryMonitorTests
+//
+//  Created by sean on 15-5-2.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BatteryMonitorTests : XCTestCase
+
+@end
+
+@implementation BatteryMonitorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
